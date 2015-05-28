84d2a534680054a029d9754c2dca15aeb288bf29
Ericson Willians Rocha Pires <ericsonwrp@gmail.com>
2015-05-28 18:49:06 -0300

    Added scopes.
    Now the language has scopes.

--- JXLInterpreter.py
+++ JXLInterpreter.py
@@ -8,7 +8,6 @@ Created on 27/05/2015
 
 from java.io import File
 from javax.xml.parsers import DocumentBuilderFactory
-from org.w3c.dom import Node;
 import sys
 
 class JXLLoader():
@@ -20,26 +19,38 @@ class JXLLoader():
         self.db_builder = self.db_factory.newDocumentBuilder()
         self.doc = self.db_builder.parse(self.file)
         self.doc.getDocumentElement().normalize()
-
+        
+class JXLScope(dict):
+    
+    def __init__(self, *args):
+        dict.__init__(self, args)
+        
+    def __getitem__(self, key):
+        return dict.__getitem__(self, key)
+    
+    def __setitem__(self, key, value):
+        dict.__setitem__(self, key, value)
+        
 class JXLReader(JXLLoader):
     
     def __init__(self, file_name):
         JXLLoader.__init__(self, file_name)
-        self.main_node_list = self.doc.getElementsByTagName("main")
-        if self.main_node_list.getLength() == 0:
-            raise RuntimeError("There's no <main> tag within the JXL file.")
-        self._global = {}
-        self.store_globals()
-        
-    def store_globals(self):
-        for i in range(self.main_node_list.getLength()):
-            node = self.main_node_list.item(i)
-            var = node.getChildNodes()
-            if var is not None:
-                for j in range(var.getLength()):
-                    var_node = var.item(j)
-                    if (var_node.getNodeType() == Node.ELEMENT_NODE):
-                        self._global[var_node.getNodeName()] = var_node.getTextContent()
+        self.outer_scopes = JXLScope()
+        self.root = self.doc.getElementsByTagName("JXL")
+        if self.root.getLength() == 0:
+            raise RuntimeError("The JXL file has no <JXL> root tag.")
+        self.children = self.root.item(0).getChildNodes()
+        for i in range(self.children.getLength()):
+            node = self.children.item(i)
+            node_name = str(node.getNodeName())
+            if node_name not in ["#text", "#comment"]:
+                self.outer_scopes[node_name] = JXLScope()
+                _vars = node.getChildNodes()
+                for j in range(_vars.getLength()):
+                    var = node.item(j)
+                    var_name = str(var.getNodeName())
+                    if var_name not in ["#text", "#comment"]:
+                        self.outer_scopes[node_name][var_name] = var.getTextContent()
         self.determine_types()
                     
     def determine_types(self):
@@ -51,10 +62,10 @@ class JXLReader(JXLLoader):
                     return int(float(s))
             except ValueError:
                 return str(s)
-        self._global = {str(name): (lambda x: extract_number(x))(value) for name, value in self._global.items()}
+        for node_name in self.outer_scopes.keys():
+            self.outer_scopes[node_name] = {str(name): (lambda x: extract_number(x))(value) for name, value in self.outer_scopes[node_name].items()}
         
 if __name__ == '__main__':
     
     jxl = JXLReader("main.xml")
-    print("JXL Global Variables: ")
-    [sys.stdout.write(name + " <- " + str(value) + "\n") for name, value in jxl._global.items()]  # @UndefinedVariable
+    print(jxl.outer_scopes)
